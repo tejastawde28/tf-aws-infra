8e2accf173bcc623533200e4b996fed8331863ef
Tejas Tawde <tawde.te@northeastern.edu>
2025-02-25 19:36:23 -0500

    check 2

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "The IDs of the public subnets"
-  value       =    aws_subnet.public[*].id
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
