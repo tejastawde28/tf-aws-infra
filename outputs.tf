0f97d099aefe5a73c0bbabb6f8b48793e6581be1
Tejas Tawde <tawde.te@northeastern.edu>
2025-03-26 17:43:57 -0400

    Remove CloudWatch Dashboard (#10)

--- outputs.tf
+++ outputs.tf
@@ -34,11 +34,6 @@ output "cloudwatch_log_group" {
   value       = aws_cloudwatch_log_group.webapp_logs.name
 }
 
-output "cloudwatch_dashboard_url" {
-  description = "URL to the CloudWatch dashboard"
-  value       = "https://${var.aws_region}.console.aws.amazon.com/cloudwatch/home?region=${var.aws_region}#dashboards:name=${aws_cloudwatch_dashboard.webapp_dashboard.dashboard_name}"
-}
-
 output "iam_role_name" {
   description = "The name of the IAM role used by the EC2 instance"
   value       = aws_iam_role.ec2_s3_role.name
